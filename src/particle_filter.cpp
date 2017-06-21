b3d0556fe144b14bf29fb8b969db3d0f2923591f
Matt Cooper <matthew_cooper@brown.edu>
2017-06-20 22:48:04 -0700

    Reduced num_particles to 200 to improve speed

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -25,7 +25,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
-  num_particles = 500;
+  num_particles = 200;
   particles.resize(num_particles);
   weights.resize(num_particles, 1);
 
